6632bf220fc196311dbe638997cb1bccf4db08b8
aaa aa <sumansai-coder>
2021-05-05 15:23:42 +0530

    mysitepro

---
+++ mysite/polls/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+# Create your views here.
+
